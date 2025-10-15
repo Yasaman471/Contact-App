0d2217f32802b58710c3d6bb3154830458ce18a9
Yasaman Abedi <Yasaman471@gmail.com>
2025-10-15 19:27:38 +0330

    feat: Initial project structure and setup basic Header component
    Set up the basic file and folder structure for the Contactify React application.
    Added the primary Header component for navigation/branding.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+  return (
+    <div>
+        <h1>Contact App</h1>
+        <p><a href="#">Botostart</a> | React.js Full Course</p>
+    </div>
+  )
+}
+
+export default Header
